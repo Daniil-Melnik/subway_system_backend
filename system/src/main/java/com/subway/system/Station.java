d1157f2e5d82098993937c7db9889532dd55a57d
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-13 23:02:10 +0300

    added Station.java class

---
+++ system/src/main/java/com/subway/system/Station.java
@@ -0,0 +1,47 @@
+package com.subway.system;
+
+public class Station {
+    private int id;
+    private String name;
+    private int line_id;
+    private int num_of_sec;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLine_id() {
+        return line_id;
+    }
+
+    public int getNum_of_sec() {
+        return num_of_sec;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId (int n) {
+        this.id = n;
+    }
+
+    public void setLine_id (int n) {
+        this.line_id = n;
+    }
+
+    public void setNum_of_sec (int n) {
+        this.num_of_sec = n;
+    }
+
+    public void setName (String s) {
+        this.name = s;
+    }
+
+    public void setStation (int id, String name, int num_of_sec, int line_id) {
+        setId(id);
+        setName(name);
+        setLine_id(line_id);
+        setNum_of_sec(num_of_sec);
+    }
+}
